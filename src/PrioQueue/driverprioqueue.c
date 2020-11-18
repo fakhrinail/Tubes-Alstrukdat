92930fd7c74e2f0cf3b69993b13aee1e3f814a9d
fakhrinail <fakhrinailwbw@gmail.com>
2020-11-19 00:01:03 +0700

    driver prioqueue + update gitignore biar ga include .exe

--- src/PrioQueue/driverprioqueue.c
+++ src/PrioQueue/driverprioqueue.c
@@ -1,9 +1,27 @@
 #include "prioqueuechar.h"
-#include "boolean.h"
+#include "../boolean.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    
+    PrioQueueChar Q;
+    MakeEmpty(&Q,1);
+    infotype uhuy;
+    uhuy.info = 'a';
+    uhuy.prio = 1;
+    Enqueue(&Q,uhuy);
+    if (IsFull(Q))
+    {
+        printf("penuh\n");
+    }
+    printf("%d\n", NBElmt(Q));
+    PrintPrioQueueChar(Q);
+    infotype X;
+    Dequeue(&Q,&X);
+    if (IsEmpty(Q))
+    {
+        printf("kosong\n");
+    }
+    PrintPrioQueueChar(Q);
     return 0;
 }
