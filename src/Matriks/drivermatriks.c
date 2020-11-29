eeadcfb40ab38720f0c6a45a088ec1be124d7cfc
grevicoc <cocabc1@gmail.com>
2020-11-30 05:04:40 +0700

    refresh

--- src/Matriks/drivermatriks.c
+++ src/Matriks/drivermatriks.c
@@ -4,9 +4,10 @@
 int main(){
     MATRIKS Map1,Map2,Map3,Map4;
     MakeMAP(&Map1,"../../doc/map1.txt");
+    MakeMAP(&Map2,"../../doc/map2.txt");
     
     
-    TulisMap(Map1);
+    TulisMap(Map2);
     printf("\n");
     
     return 0;
